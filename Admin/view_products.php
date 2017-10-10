443f3d22e09c7c7c42c5e176116832e45a5a1c59
scud74 <jamtsho@hotmail.com>
2017-10-10 08:39:07 -0500

    Add files via upload

---
+++ Admin/view_products.php
@@ -0,0 +1,46 @@
+<table width="500" align="center" bgcolor="peach">
+
+
+		<tr align ="center">
+			<td colspan="6"><h2>View All Products Here</h2></td>
+			</tr>
+				<tr align="center" bgcolor ="skyblue">
+				<th>S.N</th>
+				<th>Title</th>
+				<th>Image</th>
+				<th>Price</th>
+				<th>Edit</th>
+				<th>Delete</th>
+			</tr>
+			
+			<?php
+			include("$_SERVER[DOCUMENT_ROOT]/includes/db.php");
+			
+			$get_pro = "select * from products";
+			
+			$run_pro = mysqli_query($con, $get_pro);
+			
+			$i = 0;
+			
+			while ($row_pro=mysqli_fetch_array($run_pro)) {
+				
+			$pro_id = $row_pro['product_id'];
+			$pro_title = $row_pro['product_title'];
+			$pro_image = $row_pro['product_img1'];
+			$pro_price = $row_pro['product_price'];
+			
+			$i++;
+			?>
+		
+		<tr align="center">
+		<td><?php echo $i;?></td>
+		<td><?php echo $pro_title;?></td>
+		<td><img src="product_images/<?php echo $pro_image;?>"width="60" height="60"/></td>
+		<td><?php echo $pro_price;?></td>
+		<td><a href="index.php?edit_pro=<?php echo $pro_id;?>">Edit</td>
+		<td><a href= "delete_pro.php?delete_pro=<?php echo $pro_id;?>">Delete</td>
+		</tr>	
+
+		<?php } ?>	
+
+</table>
